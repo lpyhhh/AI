137397a69df75fec06d2f6e98c58a560c0ae82d7
lpyhhh <iphonliangpeiyu123@163.com>
2025-09-25 15:18:39 +0000

    1

--- 0-hugging_base/5-dataset.py
+++ 0-hugging_base/5-dataset.py
@@ -24,7 +24,31 @@ from datasets import load_dataset
 
 data_files = {
     "train": "/home/ec2-user/project/AI/data/drugsComTrain_raw.tsv", 
-    "test": "drugsComTest_raw.tsv"}
-drug_dataset=load_dataset("json",data_files=data_files,delimiter="\t")
-print(drug_dataset)
-#2.1 
+    "test": "/home/ec2-user/project/AI/data/drugsComTest_raw.tsv"}
+drug_dataset=load_dataset("csv",data_files=data_files,delimiter="\t")
+#print(drug_dataset)
+
+#2.1 查看小样本数据，随机抽样并看1000个 shuffle select
+drug_sample=drug_dataset["train"].shuffle(seed=42).select(range(1000))
+#print(drug_sample)
+
+#2.2 查看Unnamed: 0'列 的行数和id是否相同 
+#for split in drug_dataset.keys():
+#    assert len(drug_dataset[split])==len(drug_dataset[split].unique("Unnamed: 0"))
+#drug_dataset.keys() dict_keys(['train', 'test'])
+
+#2.3 给Unnamed: 0列改名 patient_id  rename_columns()
+drug_dataset=drug_dataset.rename_columns({"Unnamed: 0":"patient_id"})
+#print(drug_dataset)
+
+print(len(drug_dataset["train"].unique("drugName"))) # drugName
+
+#test：使用 Dataset.unique() 函数查找训练和测试集中的特定药物和病症的数量 
+#训练和测试集 药物数量，合并输出
+drug_train=drug_dataset["train"]
+drug_test=drug_dataset["test"]
+sum_drug_train=set(drug_train.unique("drugName"))
+sum_drug_test=set(drug_test.unique("drugName"))
+#.unique返回列表 set()返回集合，无序不重复
+sum_drug=len(sum_drug_test | sum_drug_train)
+print(sum_drug)
