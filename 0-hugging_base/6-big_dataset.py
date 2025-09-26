361004d78e1fc92fb02512c81c28f189c36ed11c
lpyhhh <iphonliangpeiyu123@163.com>
2025-09-26 12:22:44 +0000

    1

--- 0-hugging_base/6-big_dataset.py
+++ 0-hugging_base/6-big_dataset.py
@@ -1,3 +1,18 @@
 #/usr/bin/env python
+'''
+如何在新的文本语料库上训练一个类似于给定 checkpoint 所使用的新 tokenizer
+快速 tokenizer 的特殊功能
+目前 NLP 中使用的三种主要子词 tokenization 算法之间的差异
+如何使用🤗 Tokenizers 库从头开始构建 tokenizer 并在一些数据上进行训练
+'''
+#1 训练一个tokenzier语料库
+#/home/ec2-user/project/lpy/datasets
 
-#大数据分析
+#1.1 使用python 的语料库
+from datasets import load_dataset
+
+raw_datasets = load_dataset(
+    "json",
+    data_files="/home/ec2-user/project/lpy/datasets/python.jsonl.gz",
+    split="train"
+)
