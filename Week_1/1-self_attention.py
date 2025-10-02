6cdfed242e673a73444bea76348bb101adff49c7
lpyhhh <iphonliangpeiyu123@163.com>
2025-10-02 01:39:39 +0000

    1

---
+++ Week_1/1-self_attention.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import collections
+import torchtext
+import os
+import random
+import torch
+from torchtext.vocab import vocab, GloVe
+from tqdm import tqdm
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from torch import device
+
+########## 数据
+
+
+########## 清洗
+
+########## 构造输入
+
+########## 模型
+
+########## 流程
+
+########## 训练
+
+########## 测试
