cae8df58dbf92c5ac5c5980d24d9cafd6e85ca87
lpyhhh <iphonliangpeiyu123@163.com>
2025-10-07 02:45:37 +0000

    1

--- Week_1/test.py
+++ Week_1/test.py
@@ -14,6 +14,7 @@ from torch.autograd import Variable
 #一下是根据流程引用包
 from datasets import load_dataset
 from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import *
 
 #这些是超参数，一般是用于一个文件保存
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -34,5 +35,21 @@ test_data=imdb['test']
 ########## 清洗
 
 ########## 构造输入
-#token句子 函数定义，定义 分隔符标记
+#token句子。转化为 数字索引
 tokenizer=get_tokenizer("basic_english")
+#1 句子token化，训练集。 构建分词化，构建句子开始和结束标志
+def yield_tokens():
+    for sample in train_data:
+        yield tokenizer(sample["text"])
+
+vocab=build_vocab_from_iterator(yield_tokens(),specials=["<pad>", "<unk>"],min_freq=2)
+vocab.set_default_index(vocab["<unk>"])
+PAD_IDX=vocab["<pad>"]
+#2 
+def encode_text(text):
+    return vocab(tokenizer(text))[:512]
+#3 标签和内容转化
+def collate(batch):
+    label=torch.tensor([int(b["label"]) for b in batch])
+    seq=[torch.tensor(encode_text(b["text"])) for b in batch]
+    
